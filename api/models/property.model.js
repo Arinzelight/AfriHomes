bb71eee2c42ff45eed5fa5d9e51f20c838bdee7d
okekefeargod <okekefeargod@gmail.com>
2024-06-21 21:32:49 +0100

    this is the property model

---
+++ api/models/property.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const propertySchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        location: {
+            type: String,
+            required: true,
+        },
+        category: {
+            type: String,
+            required: true,
+            enum: ['rent', 'sale'],
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        amenities: {
+            bedrooms: {
+                type: Number,
+                required: true,
+            },
+            bathrooms: {
+                type: Number,
+                required: true,
+            },
+        },
+        picture: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+const Property = mongoose.model("Property", propertySchema);
+export default Property;
