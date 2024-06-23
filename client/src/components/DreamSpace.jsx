ac0bee28a5c1b552b33b65cc4dbd993553ad9c17
queenyaa <ysgyan@gmail.com>
2024-06-23 14:23:54 -0500

    updating the DreamSpace component to look good

--- client/src/components/DreamSpace.jsx
+++ client/src/components/DreamSpace.jsx
@@ -11,17 +11,21 @@ export default function DreamSpace() {
         <div className="flex flex-grow dreamspace">
             {/* First Column */}
             <div className="flex flex-col justify-center items-center w-1/2 dream-text">
-                <h2 className="h2-1">Make your dream space come alive</h2>
-                <p className="p2">Locate your dream environment with AfriHome. Search by Location, 
-                    Houses, Rooms and Apartment for the length of stay you are looking for.
-                </p>
-                <div className="flex-grow mx-4 searchBar2">
-                    <input type="text" placeholder="City, Location, Address, School, ZIP Code" className="search2" />
-                    <div className="flex-grow absolute inset-y-0 left-3 flex items-center pointer-events-none searchIcon2">
+                <div className="h-2/3 flex flex-col dream-text-container">
+                    <h2 className="h2-1">Make your dream space come alive</h2>
+                    <p className="p2">Locate your dream environment with AfriHome. Search by Location, 
+                        Houses, Rooms and Apartment for the length of stay you are looking for.
+                    </p>
+                </div>
+                <div className="h-1/3 flex flex-grow flex-row mx-4 searchBar2">
+                    <div className="w-3/4">
+                        <input type="text" placeholder="City, Location, Address, School, ZIP Code" className="search2" />
+                    </div>
+                    <div className="w-1/4 flex-grow flex items-center pointer-events-none searchIcon2">
                         <SearchIcon color="black" />
                     </div>
                 </div>
-            </div>
+            </div> 
 
             {/* Second Column */}
             <div className="flex w-1/2 dream-images">
