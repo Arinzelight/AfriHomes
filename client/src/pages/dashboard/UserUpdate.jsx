ac961315785990eb02e18db0d87a1aab40b66d2e
queenyaa <ysgyan@gmail.com>
2024-06-28 21:32:41 -0500

    Working on the userupdate page

---
+++ client/src/pages/dashboard/UserUpdate.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "../../components/Footer";
+
+const UserUpdate = () => {
+    return (
+        <div>
+            <Header />
+            <div></div>
+            <Footer />
+        </div>
+    );
+};
+
+export default UserUpdate;
