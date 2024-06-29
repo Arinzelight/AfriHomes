2864defd451c103fbf043985755a66dcbef12f63
queenyaa <ysgyan@gmail.com>
2024-06-29 14:24:11 -0500

    updating the Error page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import DashBoard from "./pages/dashboard/DashBoard";
 import UserUpdate from "./pages/dashboard/UserUpdate";
+import ErrorPage from "./pages/ErrorPage";
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/dashboard" element={<DashBoard />} />
         <Route path="/userupdate" element={<UserUpdate />} />
+        <Route path="/error" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
   );
