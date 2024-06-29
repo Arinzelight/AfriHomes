0fa908524a12359c62396ec93912fde031200dbe
Arinzelight <arinzelight2@gmail.com>
2024-06-29 11:45:00 +0100

    protect admin and private routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import SignIn from "./pages/SignIn";
@@ -7,19 +7,28 @@ import DashBoard from "./pages/DashBoard";
 import { Toaster } from "react-hot-toast";
 import NewPostPage from "./pages/NewPost";
 import SinglePage from "./pages/singlePage";
+import PrivateRoute from "./components/PrivateRoute";
+import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
+import Loading from "./components/loading";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Toaster />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/new-post" element={<NewPostPage />} />
-        <Route path="/single-page" element={<SinglePage />} />
-        <Route path="/dashboard" element={<DashBoard />} />
-      </Routes>
-    </BrowserRouter>
+    <Suspense fallback={<Loading />}>
+      <BrowserRouter>
+        <Toaster />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/single-page" element={<SinglePage />} />
+            <Route path="/new-post" element={<NewPostPage />} />
+          </Route>
+          <Route element={<OnlyAdminPrivateRoute />}>
+            <Route path="/dashboard" element={<DashBoard />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   );
 }
